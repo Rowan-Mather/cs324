fccbb561051880ceb2723a7996ab7a3ec4ed15c8
Rowan Mather <rowan.m.mather@gmail.com>
2023-12-14 22:23:01 +0000

    got three working

--- Lab7_WebGL_Light/Lab7_WebGL_Light/SceneSetup/SceneSetup.js
+++ Lab7_WebGL_Light/Lab7_WebGL_Light/SceneSetup/SceneSetup.js
@@ -1,4 +1,4 @@
-import * as THREE from '../../three/build/three.module.js';
+import * as THREE from '../Common/build/three.module.js';
 
 function main() {
   const canvas = document.getElementById( "gl-canvas" );
