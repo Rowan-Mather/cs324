ab1e981f14f44ff3d22cdc023ba962166cafdc99
Rowan Mather <rowan.m.mather@gmail.com>
2023-12-15 10:07:25 +0000

    lab7 lights

--- Lab7_WebGL_Light/Lab7_WebGL_Light/Interaction/Interaction.js
+++ Lab7_WebGL_Light/Lab7_WebGL_Light/Interaction/Interaction.js
@@ -1,27 +1,30 @@
 import * as THREE from '../Common/build/three.module.js';
 import { TrackballControls } from '../Common/examples/jsm/controls/TrackballControls.js';
 
-let camera, controls, scene, renderer, canvas;
+let camera0, camera1, controls, scene, renderer, canvas;
 
+var camNo = 0;
 
 function init() {
-
     
     canvas = document.getElementById( "gl-canvas" );
     const fov = 75;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
     const far = 100;
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = -10;
+    camera0 = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera0.position.z = -10;
 
-    
+    camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera1.position.x = -10;
+    camera1.position.y = 8;
+    camera1.position.z = -10;
+
     // world
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xcccccc );
     
-
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
@@ -40,16 +43,23 @@ function init() {
 
   const cubes = [
     makeInstance(geometry, 0x44abc8,  0),
-    makeInstance(geometry, 0x8244aa, -2),
+    makeInstance(new THREE.WireframeGeometry(geometry), 0x8244aa, -2),
     makeInstance(geometry, 0xFFF00,  2),
+    makeInstance(new THREE.SphereGeometry(boxWidth/2), 0xB00B69, 4),
+    makeInstance(new THREE.TorusKnotGeometry(boxWidth/4, boxWidth/10), 0x420A55, -4)
   ];
+  
     
 
     // lights
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 1.1 );
-    dirLight.position.set( 0, 10, -5 );
+    const dirLight = new THREE.DirectionalLight( 0x0000ff, 1.1 );
+    dirLight.position.set( 5, -10, -5 );
     scene.add( dirLight );
+
+    const dirLight2 = new THREE.DirectionalLight( 0xff0000, 10.1 );
+    dirLight2.position.set( -3, 5, -10 );
+    scene.add( dirLight2 );
 
     const ambientLight = new THREE.AmbientLight( 0xF22222, 2.0 );
     scene.add( ambientLight );
@@ -63,8 +73,17 @@ function init() {
 
     window.addEventListener( 'resize', onWindowResize );
 
-    createControls( camera );
+    createControls( camera0 );
     controls.update();
+
+    window.addEventListener('keydown', function(e) {
+      if(e.key=='c') {
+        camNo = 1-camNo; 
+        if (camNo) createControls( camera1 );
+        else createControls( camera0 );
+        controls.update();
+      }
+    });
 
 }
 
@@ -88,12 +107,16 @@ function createControls( camera ) {
 
 }
 
+
+
 function onWindowResize() {
 
     const aspect = window.innerWidth / window.innerHeight;
 
-    camera.aspect = aspect;
-    camera.updateProjectionMatrix();
+    camera0.aspect = aspect;
+    camera1.aspect = aspect;
+    camera0.updateProjectionMatrix();
+    camera1.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -108,7 +131,8 @@ function animate() {
 
     controls.update();
 
-    renderer.render( scene, camera );
+    if (camNo) renderer.render( scene, camera1 );
+    else renderer.render( scene, camera0 );
 
 }
 
